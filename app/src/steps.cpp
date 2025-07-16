4f66cb3ef1309c5e7b3155f40173d22ee495ecf8
Marco Widmer <marco.widmer@bytesatwork.ch>
2025-07-16 15:11:07 +0200

    app: add cucumber steps for led
    allows checking the led over the cucumber wire protocol
    
    Signed-off-by: Marco Widmer <marco.widmer@bytesatwork.ch>

--- app/src/steps.cpp
+++ app/src/steps.cpp
@@ -10,10 +10,16 @@ namespace
 {
 
 const gpio_dt_spec proximity_gpio = GPIO_DT_SPEC_GET(DT_NODELABEL(example_sensor), input_gpios);
+const gpio_dt_spec led_gpio = GPIO_DT_SPEC_GET(DT_NODELABEL(blink_led), led_gpios);
 
 void set_proximity_sensor(int enabled)
 {
 	gpio_emul_input_set(proximity_gpio.port, proximity_gpio.pin, enabled);
+}
+
+int get_led_state(void)
+{
+	return gpio_emul_output_get(led_gpio.port, led_gpio.pin);
 }
 
 void wait_for_main_tick(void)
@@ -21,11 +27,34 @@ void wait_for_main_tick(void)
 	k_sleep(K_TIMEOUT_ABS_MS(ROUND_UP(k_uptime_get() + 1, MAIN_LOOP_PERIOD_MS + 1)));
 }
 
+void check_led_state(bool state, std::uint64_t duration_ms)
+{
+	k_msleep(TIME_SLACK_MS - 1);
+
+	ASSERT_EQ(state, get_led_state());
+	k_msleep(duration_ms - 2 * TIME_SLACK_MS - 1);
+	ASSERT_EQ(state, get_led_state());
+
+	k_msleep(TIME_SLACK_MS - 1);
+}
+
 WHEN("^I trigger the proximity sensor$")
 {
 	set_proximity_sensor(1);
 	wait_for_main_tick();
 	set_proximity_sensor(0);
+}
+
+THEN("^the LED is on for (\\d+) milliseconds?$")
+{
+	REGEX_PARAM(std::uint64_t, ms);
+	check_led_state(1, ms);
+}
+
+THEN("^the LED is off for (\\d+) milliseconds?$")
+{
+	REGEX_PARAM(std::uint64_t, ms);
+	check_led_state(0, ms);
 }
 
 }
